198c26b9885d780f4cf8a4a1ef742173143dddc2
dhextras <mohammedsafki27@gmail.com>
2024-12-01 23:08:33 +0530

    feat: aoc 2024 - day 1 - part 2

--- day_1.py
+++ day_1.py
@@ -1,11 +1,10 @@
-##### Part 1 #####
-
 with open("inputs/day_1_input.txt", "r") as f:
     data = f.readlines()
 
 group1 = [line.split("   ")[0].strip() for line in data]
 group2 = [line.split("   ")[1].strip() for line in data]
 
+##### Part 1 #####
 sorted_g1 = sorted(group1)
 sorted_g2 = sorted(group2)
 
@@ -17,4 +16,13 @@ print(differences)
 
 
 ##### Part 1 #####
-# TODO:
+similarity_score = 0
+sorted_g2_obj = {}
+
+for i in group2:
+    sorted_g2_obj[i] = sorted_g2_obj.get(i, 0) + 1
+
+for j in group1:
+    similarity_score += int(j) * sorted_g2_obj.get(j, 0)
+
+print(similarity_score)
